35b995e88ac94af76c2a178b139c98658ceb6b3c
Ertan Kaya <107481123+ertantherock@users.noreply.github.com>
2022-10-02 15:11:53 +0300

    Add files via upload

---
+++ scanner/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double b;
+        System.out.println("B sayısını giriniz: ");
+        b = input.nextInt();
+
+        System.out.println(b);
+
+
+
+    }
+}
